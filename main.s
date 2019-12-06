164557d44e34f289c8de0060eb311bd8e139c29f
vapvin <tpehdj12@daum.net>
2019-12-06 18:09:49 +0900

    Good

--- main.s
+++ main.s
@@ -1,9 +1,19 @@
 section .data
 	msg db "Hello World"
+	msg db "What is Assembly?"
 section .text
 	global_start
 
 _start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 12
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
