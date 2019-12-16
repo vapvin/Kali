7014bbd4214be7525629aa3ea7da43b49fceec68
vapvin <tpehdj12@daum.net>
2019-12-17 00:06:00 +0900

    Kali Linux

--- mains.c
+++ mains.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-	int a[3][3] = {{1,2,3,},{4,5,6,},{7,8,9}};
 
 int main(void) {
-	int i, j;
-
-	for(i = 0; i < 3; i++){
-		for(j = 0; j < 3; j++) {
-			printf("%d", a[i][j]);
-		}
-	}
+	int a[5] = {1,2,3,4,5};
+	int *b = a;
+	printf("%d\n", b[2]);
 
 	return 0;
 }
