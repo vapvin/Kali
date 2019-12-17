fd17721a1de93463ba7d2976c8fbe9a071faff71
vapvin <tpehdj12@daum.net>
2019-12-17 10:42:51 +0900

    This Add Memory

--- study.c
+++ study.c
@@ -1,23 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int a[] = {
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-    };
-
-    int *b = a;
-
-    printf("%d", b[2]);
+    int *a = malloc(sizeof(int));
+    printf("%d", a);
+    *a = malloc(sizeof(int));
+    printf("%d", a);
 
     return 0;
 }
