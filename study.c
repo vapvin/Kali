8d7d4f5a72f130699dc18ce82309a072500e1e08
vapvin <tpehdj12@daum.net>
2019-12-18 12:31:14 +0900

    Input File

--- study.c
+++ study.c
@@ -1,23 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct
 {
-    char studentId[10];
-    char name[10];
-    int grade;
-    char major[100];
+    char name[20];
+    int score;
 } Student;
 
 int main(void)
 {
-    Student *s = malloc(sizeof() Student);
-
-    printf("%s\n", s->studentId);
-    printf("%s\n", s->name);
-    printf("%d\n", s->grade);
-    printf("%s\n", s->major);
 
     return 0;
 }
