ae88ef03fd9b3ed67790d70437d1a86cd75b61b5
vapvin <tpehdj12@daum.net>
2019-12-18 12:12:40 +0900

    Struct

--- study.c
+++ study.c
@@ -1,20 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-int add(int a, int b)
+struct Student
 {
-    return a + b;
-}
-
-int (*process(char *a))(int, int)
-{
-    printf("%s\n", a);
-    return add;
-}
+    char studentId[10];
+    char name[10];
+    int grade;
+    char major[100];
+};
 
 int main(void)
 {
-    printf("%d", process("10과 20을 더한 값을 출력하여 보겠습니다.")(10, 20));
+    struct Student s;
+    strcpy(s.studentId, "20190317");
+    strcpy(s.name, "문현규");
+    strcpy(s.major, "Computer");
+    s.grade = 98;
+
+    printf("%s\n", s.studentId);
+    printf("%s\n", s.name);
+    printf("%d\n", s.grade);
+    printf("%s\n", s.major);
 
     return 0;
 }
