ddf9e8d26dad0b4665bf8d10694133c46bcdf6c0
vapvin <tpehdj12@daum.net>
2019-12-19 17:21:12 +0900

    Function of Start

--- loop.s
+++ loop.s
@@ -22,3 +22,17 @@ done:
 	mov rax, 60
 	mov rdi, 0
 	syscall
+
+section .data
+	msg db "A"
+section .text
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 1
+	mov r10, 1
+
+	
