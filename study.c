81596db4336f849d1215bdfb064631640a5cd8ba
vapvin <tpehdj12@daum.net>
2019-12-21 20:56:37 +0900

    This Echo Program for DBG

--- study.c
+++ study.c
@@ -6,9 +6,9 @@
 
 int main(void)
 {
-    char a[SIZE];
+    char a[SIZE]; // 매크로를 통한 사이즈 배열의 크기를 1000만큼 할당해 줌
 
-    scanf("%s", &a[SIZE]);
-    printf("%s", a[SIZE]);
+    scanf("%s", &a[SIZE]); // 스캔을 통해 값을 입력받고
+    printf("%s", a[SIZE]); // 입력받은 값을 출력
     return 0;
 }
