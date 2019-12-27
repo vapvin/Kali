7fdb1f82d826e939f4406fe729352c55183ee116
vapvin <tpehdj12@daum.net>
2019-12-27 17:59:55 +0900

    16bit -> 64bit register

--- algorithm.s
+++ algorithm.s
@@ -19,12 +19,12 @@ stacktop:
 
         segment code
 start:
-        mov ax,data
-        mov ds,ax
-        mov ax,stack
-        mov ss,ax
-        mov sp,stacktop
-          mov bp,stacktop
+        mov rax,data
+        mov ds,rax
+        mov rax,stack
+        mov ss,rax
+        mov rsp,stacktop
+        mov rbp,stacktop
 
         
         mov ah,0h
@@ -32,19 +32,19 @@ start:
         int 10h
 
        
-        mov ax,word 200
-        push ax        
-        mov ax,word 50
-        push ax          
-        mov ax,word 20
-        push ax          
-        mov ax,word 65
-        push ax          
+        mov rax,word 200
+        push rax        
+        mov rax,word 50
+        push rax          
+        mov rax,word 20
+        push rax          
+        mov rax,word 65
+        push rax          
         call line
 
         
 
-        xor ax,ax
+        xor rax,rax
         int 16h
 
         
@@ -63,37 +63,37 @@ start:
 line:
         push bp
         mov bp,sp
-        mov ax,[bp+4] 
-        mov [y2],ax
-        mov ax,[bp+6]
-        mov [x2],ax
-        mov ax,[bp+8]
-        mov [y1],ax
-        mov ax,[bp+10]
-        mov [x1],ax
+        mov rax,[bp+4] 
+        mov [y2],rax
+        mov rax,[bp+6]
+        mov [x2],rax
+        mov rax,[bp+8]
+        mov [y1],rax
+        mov rax,[bp+10]
+        mov [x1],rax
 
         
 
-        mov ax,[x2]
-        sub ax,[x1]
-        cmp ax,0
+        mov rax,[x2]
+        sub rax,[x1]
+        cmp rax,0
         jnl abs1
-        neg ax
-abs1:   mov [dex],ax
+        neg rax
+abs1:   mov [dex],rax
 
         
 
-        mov ax,[y2]
-        sub ax,[y1]
-        cmp ax,0
+        mov rax,[y2]
+        sub rax,[y1]
+        cmp rax,0
         jnl abs2
-        neg ax
-abs2:   mov [dy],ax
+        neg rax
+abs2:   mov [dy],rax
 
         
 
-        mov ax,[x1]
-        cmp ax,[x2]
+        mov rax,[x1]
+        cmp rax,[x2]
         jl min
         mov [x_add],word -1
         jmp end
@@ -101,8 +101,8 @@ min:    mov [x_add],word 1
 end:
         
 
-        mov ax,[y1]
-        cmp ax,[y2]
+        mov rax,[y1]
+        cmp rax,[y2]
         jl min2
         mov [y_add],word -1
         jmp end2
@@ -110,54 +110,54 @@ min2:   mov [y_add],word 1
 end2:
         
 
-        mov ax,[dy]
-        cmp ax,[dex]
+        mov rax,[dy]
+        cmp rax,[dex]
         jl min3
-        mov ax,[dy]
-        mov [maxd],ax
+        mov rax,[dy]
+        mov [maxd],rax
         jmp end3
-min3:   mov ax,[dex]
-        mov [maxd],ax
+min3:   mov rax,[dex]
+        mov [maxd],rax
 end3:
 
-        mov cx,[maxd]
-here:   mov ax,[dy]
-        add [act_dy],ax
-        mov ax,[act_dy]
-        cmp ax,[maxd]
+        mov rcx,[maxd]
+here:   mov rax,[dy]
+        add [act_dy],rax
+        mov rax,[act_dy]
+        cmp rax,[maxd]
         jl next
-        mov ax,[maxd]
-        sub [act_dy],ax
-        mov ax,[y_add]
-        add [y1],ax
-next:   mov ax,[dex]
-        add [act_dx],ax
-        mov ax,[act_dx]
-        cmp ax,[maxd]
+        mov rax,[maxd]
+        sub [act_dy],rax
+        mov rax,[y_add]
+        add [y1],rax
+next:   mov rax,[dex]
+        add [act_dx],rax
+        mov rax,[act_dx]
+        cmp rax,[maxd]
         jl next2
-        mov ax,[maxd]
-        sub [act_dx],ax
-        mov ax,[x_add]
-        add [x1],ax
+        mov rax,[maxd]
+        sub [act_dx],rax
+        mov rax,[x_add]
+        add [x1],rax
 next2:  call plot
         loop here
-        mov sp,bp
-        pop bp
+        mov rsp,rbp
+        pop rbp
         ret 8
 
 plot:
-        push ax
-        push bx
-        push cx
-        push dx
+        push rax
+        push rbx
+        push rcx
+        push rdx
         mov ah,0ch
-        mov dx,[y1]
-        mov cx,[x1]
+        mov rdx,[y1]
+        mov rcx,[x1]
         mov al,0d0h     
         mov bh,0h
         int 10h
-        pop dx
-        pop cx
-        pop bx
-        pop ax
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
         ret 
