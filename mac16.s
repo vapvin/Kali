0cba676d4d8124943e4b90e44fbcdd4ed05989bc
vapvin <tpehdj12@daum.net>
2020-02-23 14:12:23 +0900

    Mac 16 => Assembly

---
+++ mac16.s
@@ -0,0 +1,18 @@
+section .data
+	db msg "Hello World"
+
+section .text
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 16
+
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+
+	syscall
